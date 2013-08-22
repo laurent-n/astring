a83d2bb110d3c1fd59d99f0102957a4077f28202
lnaltidev@gmail.com <lnaltidev@gmail.com@0681aa88-e5dc-6374-5baf-152c7da8008c>
2013-08-22 19:09:04 +0000

    Initial

---
+++ trunk/ astring/UT_BCB_XE2/Test/AStringUTTests.cpp
@@ -0,0 +1,39 @@
+//---------------------------------------------------------------------------
+// DUnit Project File.
+//   Entry point of C++ project using DUnit framework.
+//---------------------------------------------------------------------------
+#include <System.hpp>
+#pragma hdrstop
+
+#include <tchar.h>
+#include <vcl.h>
+#include <GUITestRunner.hpp>
+
+
+
+
+
+WINAPI _tWinMain(HINSTANCE, HINSTANCE, LPTSTR, int)
+{
+  try
+  {
+	 Application->Initialize();
+	 Guitestrunner::RunRegisteredTests();
+  }
+  catch (Exception &exception)
+  {
+	 Application->ShowException(&exception);
+  }
+  catch (...)
+  {
+	 try
+	 {
+	   throw Exception("");
+	 }
+	 catch (Exception &exception)
+	 {
+	   Application->ShowException(&exception);
+	 }
+  }
+  return 0;
+}
